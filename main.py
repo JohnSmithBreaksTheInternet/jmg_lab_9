022a25d61cc24fa9c9f33c93e9ff20f11a1ce76c
JohnSmithBreaksTheInternet <jmilton.gillis@ufl.edu>
2024-04-10 17:14:24 -0400

    made changes

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-def encode_password(password):
-    encoded_password =" "
-    for digit in password:
-        new_digit = str((int(digit)+3)%10)
-        encoded_password += new_digit
-    return encoded_password
-
-def decode_password(password):
-    decoded_password =" "
-    for digit in password:
-        new_digit = str((int(digit)-3)%10)
-        decoded_password += new_digit
-    return decoded_password
-
 def menu():
     print("Menu")
     print("-------------")
